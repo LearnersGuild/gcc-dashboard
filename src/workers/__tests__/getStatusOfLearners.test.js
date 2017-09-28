7a9450d4844fc312eadb2642529a0dd7d8d31cf8
Aaron Xavier <aarondxavier@gmail.com>
2017-09-28 14:14:51 -0700

    Tests for getStatusOfLearners formatContacts

--- src/workers/__tests__/getStatusOfLearners.test.js
+++ src/workers/__tests__/getStatusOfLearners.test.js
@@ -6,22 +6,43 @@ import {
   properties, lists
 } from '../utils/report'
 
-// test('workers/getStatusOfLearners', t => {
-//   t.test('formatContacts', tt => {
-//     tt.test('should rereturn an array of records', ttt => {
-//       ttt.plan(1)
+const contacts = [
+  {'canonical-vid': '1', 
+    email: 'email@test.com',
+    properties: {
+    'cancellation_date': {value: '1474243200000'},
+    'resignation_date': {value: '1464988764846'}
+    }
+  },
+  {'canonical-vid': '2', 
+    email: 'email2@test.com',
+    properties: {
+    'cancellation_date': {value: '1464988764846'},
+    'resignation_date': {value: '1474243200000'}
+    }
+  }
+]
 
-//       ttt.equal(clearData['email@test.com'].properties[0].property, 'has_pif', 'should be has_pif')
-//     })
+test('workers/getStatusOfLearners', t => {
+  t.test('formatContacts', tt => {
+    const records = formatContacts(contacts, lists[13][2592], 2592)
+    tt.test('should return an array of 2 records', ttt => {
+      ttt.plan(2)
+      ttt.equal(Array.isArray(records), true, 'should be true')
+      ttt.equal(records.length, 2, 'should be 2')
+      
+    })
 
-//     tt.test('should return a new object with the addition of each email', ttt => {
-//       ttt.plan(3)
-//       const oneEmail = clearHubspotData({}, 'email@test.com')
-//       const twoEmail = clearHubspotData(oneEmail, 'email2@test.com')
+    tt.test('should receive properties from the list object', ttt => {
+      ttt.plan(1)
+      ttt.equal(records[0].stageStatus, 'Out During Stage', 'should be \'Out During Stage\'')
+    })
+    tt.test('should assign the proper stages to each record', ttt => {
+      ttt.plan(2)
+      ttt.equal(records[0].stage, 'Program Start prior to Commitment', 'should be \'Program Start prior to Commitment\'')
+      ttt.equal(records[1].stage, 'Crossed Commitment to End Phase 1', 'should be \'Crossed Commitment to End Phase 1\'')
+      
 
-//       ttt.equal(twoEmail.hasOwnProperty('email@test.com'), true, 'should be true')
-//       ttt.equal(twoEmail.hasOwnProperty('email2@test.com'), true, 'should be true')
-//       ttt.equal(oneEmail.hasOwnProperty('email2@test.com'), false, 'should be false')
-//     })
-//   })
-// })
+    })
+  })
+})
