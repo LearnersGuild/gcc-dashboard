92bd22f76110df9f84cae0bdf78a841e748d1e60
Aaron Xavier <aarondxavier@gmail.com>
2017-11-06 14:38:39 -0800

    WIP on Performance Report

---
+++ src/controllers/createPerformanceReport.js
@@ -0,0 +1,26 @@
+'use strict'
+require('dotenv').config()
+const moment = require('moment')
+const knex = require('../db')
+const _ = require('lodash')
+
+date_phase_1
+date_phase_2
+date_phase_3
+date_phase_4
+date_phase_5
+
+phase_1_attempt 
+phase_2_attempt
+phase_3_attempt
+phase_4_attempt
+// First, Second, Third, Fourth, Fifth
+
+phase_1_interview_outcome
+phase_2_interview_outcome
+phase_3_interview_outcome
+phase_4_interview_outcome
+// Accept, Not Accept
+
+
+
