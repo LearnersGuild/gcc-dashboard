2039d625f323bd26e0f0557a4db81edc63f10c59
Aaron Xavier <aarondxavier@gmail.com>
2017-11-08 18:16:03 -0800

    Performance Report Has Not Advanced logic complete

--- src/controllers/createPerformanceReport.js
+++ src/controllers/createPerformanceReport.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 const moment = require('moment')
 const knex = require('../db')
 const _ = require('lodash')
+// const demoSegments = require('./utils/performanceReport').demoSegments
+// const template = require('./utils/performanceReport').template
+const utils = require('./utils/performanceReport')
 
 const fields = [
   'date_phase_1',
@@ -22,58 +25,90 @@ const fields = [
   'race',
   'two_or_more_races',
   'enrollee_start_date',
-  'phase'
+  'phase',
+  'exit_type',
+  'exit_phase',
+  'metaStage'
 ]
-
-const phases = [
-  'phase_1',
-  'phase_2',
-  'phase_3',
-  'phase_4',
-  'phase_5'
-]
-
-
-let totalPhaseAssessmentAttempts = 0
-
 // First, Second, Third, Fourth, Fifth
 // Accept, Not Accept
 
 const hasNotAdvanced = (learner, phase) => {
-  if (learner[`phase_${phase + 1}_interview_outcome` === 'Not Accept']) {
-    return true
-  } else if (learner.phase === `Phase ${phase}` && !learner[`phase_${phase + 1}_interview_outcome`]) {
-    return true
+  if (phase === 5) {
+    learner.phase === 'Phase 5' ? true : false
+  } else if (phase === 4) {
+    learner.phase === 'Phase 4' ? true : false
   } else {
-    return false
+    if (learner[`phase_${phase + 1}_interview_outcome` === 'Not Accept']) {
+      return true
+    } else if (learner.phase === `Phase ${phase}` && !learner[`phase_${phase + 1}_interview_outcome`]) {
+      console.log(learner[`phase_${phase + 1}_interview_outcome`])
+      return true
+    } else {
+      return false
+    }
   }
 }
 
-// const getPhase1Data = (learnerData, type) => {
-//   const weeks = []
-//   const tries = []
-//   const phase1 = [
-//     {
-//       segment: 'Man',
-//       lessThan6: _.filter(learnerData, learner => { 
-//         if (learner.gender === 'Man' && learner.date_phase_2) {
-//           if (learner.date_phase_1) {
-//             return Math.round(moment(learner.date_phase_1).diff(moment(learner.date_phase_2), 'days')/7) < 6 
-//           } else {
-//             return Math.round(moment(learner.enrollee_start_date).diff(moment(learner.date_phase_2), 'days')/7) < 6 
-//           }
-//         } else {
-//           return false
-//         }
-//       }).length,
-//       in6: 0,
-//       in7or8: 0,
-//       moreThan8: 0,
-//       in1Try: 0,
-//       moreThan1Try: 0
-//     }
-//   ]
-// }
+const createContainer = async (segments) => {
+  let segmentContainer = []
+  
+  segments.forEach(segment => {
+    let segmentObject = Object.assign({}, utils.template)
+    segmentObject.segment = segment
+    segmentContainer.push(segmentObject)
+  })
+  return segmentContainer
+}
+
+const createTableData = async (cohorts, learnerData) => {
+  const phases = utils.phases
+  const cohort = await createContainer(cohorts)
+  const demo = await createContainer(utils.demoSegments)
+  
+  let totalAssessmentAttempts = 0
+  let totalAssessmentAdvancements = 0
+
+  learnerData.forEach(learner => {
+    let gender
+    if (learner.gender === 'Transgender' || learner.gender === 'Other') {
+      gender = 'Transgender or Other'
+    } else if (learner.gender !== 'Man' && learner.gender !== 'Woman') {
+      gender = 'Gender Undefined'
+    } else {
+      gender = learner.gender
+    }
+    let genderIndex = utils.demoSegments.indexOf(gender)
+    let raceIndex = utils.demoSegments.indexOf(learner.race) === -1 ? utils.demoSegments.indexOf('Race Undefined') : utils.demoSegments.indexOf(learner.race)
+    let twoOrMoreRacesIndex = utils.demoSegments.indexOf('Two or More Races')
+    let cohortIndex = cohorts.indexOf(moment(learner.enrollee_start_date).format('YYYY-MM'))
+
+    phases.forEach(phase => { 
+      if (hasNotAdvanced(learner, phase)) {
+        cohort[cohortIndex][`phase${phase}HasNotAdvanced`]++
+        cohort[cohortIndex]['totalHasNotAdvanced']++
+        demo[genderIndex][`phase${phase}HasNotAdvanced`]++
+        demo[genderIndex]['totalHasNotAdvanced']++
+        demo[raceIndex][`phase${phase}HasNotAdvanced`]++
+        demo[raceIndex]['totalHasNotAdvanced']++
+        if (learner.two_or_more_races) {
+          demo[twoOrMoreRacesIndex][`phase${phase}HasNotAdvanced`]++
+          demo[twoOrMoreRacesIndex]['totalHasNotAdvanced']++
+        }
+      // } else {
+
+      //   isLessThan6(learner, phase)
+      //   in6(learner, phase, segment)
+      //   in7or8(learner, phase, segment)
+      //   moreThan8(learner, phase, segment)
+      //   in1Try(learner, phase, segment)
+      //   moreThan1Try(learner, phase, segment)
+      //   advancedTries(learner, phase, segment)
+      //   advanced(learner, phase, segment)
+      }
+    })
+  })
+}
 
 const getCohorts = () => {
   return knex('status_of_learners').select(knex.raw('to_char(enrollee_start_date, \'YYYY-MM\')'))
@@ -90,12 +125,12 @@ const getLearnerData = () => {
   return knex.select(...fields).from('status_of_learners')
     .where('created_at', '>=', moment().subtract(8, 'hours').format('YYYY-MM-DD'))
     .then(learners => {
-      //do the thing
+      return learners
     })
     .catch(err => { console.log(err) })
 }
 
-export const report = async (cb) => {
+const report = async (cb) => {
   let cohorts = await getCohorts()
   // [ '2016-07',
   // '2016-09',
@@ -106,9 +141,11 @@ export const report = async (cb) => {
   // '2017-05',
   // '2017-08',
   // '2017-09' ]
-  // let learnerData = await getLearnerData()
+  let learnerData = await getLearnerData()
+  createTableData(cohorts, learnerData)
   // let reportData = {}
   // cb(reportData)
 }
 
+report()
 
